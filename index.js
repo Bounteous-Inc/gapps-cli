0134ff6efa6c517bbfca01557475b13ac2bd8fe8
Dan Wilkerson <dwilkerson@lunametrics.com>
2017-04-25 11:28:13 -0400

    Fetch and push working

--- index.js
+++ index.js
@@ -1,131 +1,61 @@
 #!/usr/bin/env node
+// THIS WON'T WORK WITH "bound" FILES 
 'use strict'
 let argv = require('minimist')(process.argv.slice(2));
 
 const google = require('googleapis')
-const prompt = require('prompt')
-const minimist = require('minimist')
-const express = require('express')
 const fs = require('fs')
-const async = require('async')
-const Config = require('./lib/Config.js')(configFilePath)
-const CliInterface = require('./lib/CliInterface.js')
-const GoogleAuth2 = require('./lib/GoogleOAuth2.js')
+const CLI = require('./lib/Cli.js')
+const homedir = require('os').homedir();
+const generateToken = require('./lib/TokenGenerator.js');
+const configPath = homedir + '/.gappscli/config.json';
+const tokenPath = homedir + '/.gappscli/token.json';
 
-// TODO let these be command line arg and save them
-// Check required configs
-async.each([
-  checkToken,
-  check('fileId'),
-  checkKeys
-], (err, result) => {
+let scope = ['drive', 'drive.file', 'drive.scripts'].map((name) => 'https://www.googleapis.com/auth/' + name)
+let localConfig;
+let config;
+let tokens;
 
-  if (err) return console.log(err.message)
+try {
 
-  CliInterface(argv)
+  config = require(configPath);
+  config.scope = scope;
 
-})
+} catch(e) {
 
-/**
- * @name checkToken
- * @description
- * Checks to see if a token property exists in the config
- *
- * @arg callback {function}
- */
-function checkToken(callback) {
-    
-  if (Config.token.get()) return callback(null)
+  console.error(`Please configure your config.json in ${configPath}`);
+  
+}
 
-  // Token can be passed in as an argument
-  if (argv.token) {
+try {
 
-    Config.token.set({access_token: argv.token})
-    return callback(null)
+  tokens = require(tokenPath);
 
-  }
+} catch(e) {
 
-  GoogleOAuth2.generateToken(function(err, tokens) {
+  generateToken(config, function(err, tokens) {
 
-    if (err) return callback(err)
+    fs.writeFileSync(tokenPath, JSON.stringify(tokens));
+    return init(tokens);
 
-    Config.token.set(tokens)
-    callback(null)
-
-  })
+  });
 
 }
 
-/**
- * @name checkKeys
- * @description
- * Checks for a path to a clientSecret and clientId passed in as a command
- * line argument
- *
- * @arg callback {function}
- */
-function checkKeys(callback) {
+if (tokens) init(tokens);
 
-  if (!argv.keys) {
 
-    prompt.get('Keys file path', (err, result) => {
+function init(tokens) {
 
-      if (err) return callback(err)
-      
-      callback(null, getConfig_(path))
+  let OAuth2 = google.auth.OAuth2;
+  let redirectUri = 'http://localhost:8080/redirect';
+  let client = new OAuth2(config.clientId, config.clientSecret, redirectUri);
 
-    })
-
-  } else {
-
-    callback(null, getConfig_(argv.keys))
-
-  }
-
-  function getConfig_(path) {
-
-    return JSON.parse(fs.readFileSync(path))
-
-  }
-
-}
-
-/**
- * @name check
- * @description
- * Factory that returns function for checking/setting required properties
- *
- * @arg name {string} name of configuration property
- * @returns {function} function to check property or request it
- */
-function check(name) {
-
-  return function checkProp_(callback) {
-
-    if (Config[name].get()) return callback(null)
-
-    if (argv[name]) {
-
-      Config[name].set(arg[name])
-      return callback(null)
-
-    }
-
-    let promptProps = { properties: {} }
-
-    promptProps[name] = {
-      description: `What is the ${name} of the project?`
-    }
-
-    prompt.get(promptProps, (err, result) => {
-
-      if (err) return callback(err)
-
-      Config[name].set(result)
-      callback(null)
-
-    })
-
-  }
+  client.setCredentials(tokens);
+  
+  CLI(argv, google.drive({
+    'version': 'v3',
+    'auth': client
+  }));
 
 }
